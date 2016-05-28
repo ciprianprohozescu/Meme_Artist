ebaaab94f492a7c7bf55db3403ca09b4db668076
Ciprian P <ciprianprohozescu@gmail.com>
2016-05-28 12:40:19 +0300

    Added colourful background and quotes.

--- app/src/main/java/com/cyworks/memeartist/StartPage.java
+++ app/src/main/java/com/cyworks/memeartist/StartPage.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class StartPage extends AppCompatActivity {
 
@@ -11,6 +14,43 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        TextView startText = (TextView) findViewById(R.id.startText);
+
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(10);
+        switch (randomInt) {
+            case 0:
+                startText.setText("Be creative! :)");
+                break;
+            case 1:
+                startText.setText("If you are unsure how to use a meme, ALWAYS look it up on the Internet.");
+                break;
+            case 2:
+                startText.setText("\"Not all that glitters is gold\" -William Shakespeare \n\"Not all that is gold glitters\" -J.R.R.Tolkien");
+                break;
+            case 3:
+                startText.setText("\"Art should terrify\" -a video game character \nMemes should terrify too.");
+                break;
+            case 4:
+                startText.setText("A meme a day keeps the lols close... Rhymes are overrated.");
+                break;
+            case 5:
+                startText.setText("Always share your memes with the world. Unless they suck, of course.");
+                break;
+            case 6:
+                startText.setText("How do memes grow? They grow out of your life. \n-adapted from Robert Penn Warren");
+                break;
+            case 7:
+                startText.setText("\"The poet is a liar who always speaks the truth.\" -Jean Cocteau");
+                break;
+            case 8:
+                startText.setText("\"You can't blame gravity for falling in love\" -Albert Einstein");
+                break;
+            case 9:
+                startText.setText("\"Words are but pictures of our thoughts.\" -John Dryden \nSo are memes.");
+                break;
+        }
     }
 
     public void gotoSelector(View view) {
